10615200ae3c99c0b9da220e5eed59a313004cd3
m.tighzaoui <tighzaoui@outlook.com>
2021-11-29 15:11:32 +0000

    configration redux thunk and add reselect lib

---
+++ src/redux/reducers/appReducer.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = {
+  images: [],
+};
+
+const appReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default appReducer;
